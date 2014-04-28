a64a5638479263d99eab23dd7b9e01d9307986b3
VDSDmitry <vdsdmitry@gmail.com>
2014-04-28 23:04:13 +0000

    Firefox building is now supported... but "voices links" (prj specified) do not work

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 # automatic extension building script
 
 
@@ -6,44 +6,60 @@
 chrome="`which chrome || which chromium`"
 firefox="`which firefox`"
 #constants
-jquery_file="build/jquery-2.1.0.min.js"
-jquery_link="http://ajax.googleapis.com/ajax/libs/jquery/2.1.0/jquery.min.js"
+jquery_file="jquery-2.1.0.min.js"
+
 path=`pwd`
-name="godville-ui+"
-tmp_dir="$path/build/godville_ui"
+build_path="$path/build"
+name="godville_ui"
+#при смене tmp_path перемещать расширение chrome в build_path!!! 
+tmp_path="$build_path/$name"
 
-if [ "$chrome" ]; then
-	mkdir -p build
-	mkdir -p $tmp_dir
-	rm -f build/$name.crx
-	cp *.js *.json *.css *.png $tmp_dir
-	cp -r images $tmp_dir
-
-	# if the developer key exists add the key parameter 
-	key=`find $path/build/*.pem | head -1`
-	if [[ $key != *"No such file or directory"* ]]; then
-		key_param="--pack-extension-key=$key";
-	fi	
-	
-	# if jquery library does not exists download it
-	if [ ! -f $jquery_file ]; then
-		echo "...trying to download jquery library..."
-		wget $jquery_link  -q -O $jquery_file
-		if [ -f $jquery_file ]; then
-			echo "Jquery library has been successfully downloaded to the build directory!"
-		else
-			echo "downloading was unsuccessful!"				
-		fi
-	fi
-	# if jquery library exists pack the extension
-	if [ -f $jquery_file ]; then
-		cp $jquery_file $tmp_dir
-		$chrome --pack-extension=$tmp_dir $key_param --no-message-box
-		rm -r $tmp_dir
-	else	
-		echo "Please download jquery-2.1.0.min.js and put it to the build directory!"
-	fi
+# check jquery library 
+if [ ! -f $jquery_file ]; then
+	echo "Please download jquery-2.1.0.min.js and put it to the build directory!"	
+	exit 1
 fi
-if [ $firefox != '' ]; then
-    echo compilation $firefox
+
+
+if [ -n "$chrome" ]; then
+	rm -rf $tmp_path
+	mkdir -p $build_path
+	mkdir -p $tmp_path
+	rm -f $build_path/$name.crx
+
+	cp *.js *.json *.css *.png $tmp_path
+	cp -r images $tmp_path
+	
+	# if the developer key exists add the key parameter 
+	key=`find ./ -name "*.pem" -print -quit`
+	if [ -n "$key" ]; then
+			key_param="--pack-extension-key=$key";
+			echo "Using developer key: $key"
+	fi	
+					
+	cp $jquery_file $tmp_path
+	$chrome --pack-extension=$tmp_path $key_param --no-message-box
+	rm -r $tmp_path
+
+fi
+
+if [ -n "$firefox" ]; then
+    echo "\nPacking firefox extension..."
+    rm -rf $tmp_path
+    mkdir $tmp_path
+	cp -r firefox/content/ $tmp_path/
+ 	cp -n *.js *.css *.png $tmp_path/content/
+ 	cp -r images $tmp_path/content/
+ 	cp logo48.png $tmp_path/icon.png
+ 	cp firefox/icon64.png $tmp_path/content/images/icon64.png
+ 	rm -f $tmp_path/content/images/favicon_dummy.png
+ 	
+	cp firefox/chrome.manifest firefox/install.rdf $tmp_path/
+	cd $tmp_path
+	rm -f $build_path/$name.xpi
+	zip -qr $build_path/$name.xpi .
+	rm -rf $tmp_path
+	if [ -f $build_path/$name.xpi ]; then
+		echo "Successfully packed!\nOutput file: $build_path/$name.xpi"
+	fi
 fi
