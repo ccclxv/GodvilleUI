e9319fe9fd8fe19f210366f1090aa999845b4d8b
VDSDmitry <vdsdmitry@gmail.com>
2014-05-01 00:15:29 +0000

    приведен к виду модуля.

--- logger.js
+++ logger.js
@@ -9,50 +9,53 @@
 // Modifications:
 // AUOE: code simplification  27.04.14
 
+// !requires ui_data.isArena
 
-var Logger = function() {
-	this.updating = false;
+
+var Logger = {
+	updating : true,
+	need_separator : false,
+	elem : null,
 	
-	
-	var need_separator = false;
-	var elem = $('<ul id="stats_log" />');
-	$('#menu_bar').after(elem);
-	elem.append('<div id="fader" style="position: absolute; left: 0; float: left; width: 50px; height: 100%;" />');
-	
+	create : function(){
+		this.elem = $('<ul id="stats_log" />');
+		$('#menu_bar').after(this.elem);
+		this.elem.append('<div id="fader" style="position: absolute; left: 0; float: left; width: 50px; height: 100%;" />');
+	},
 	
 	
 	// Updates logger data
-	this.update = function() {
+	update : function() {
 		if (ui_data.isMap) {
-			updateStats('dungeon');
+			this.updateStats('dungeon');
 		}
 		if (ui_data.isArena && !ui_data.isMap) {
-			updateStats('arena');
+			this.updateStats('arena');
 		}
-		updateStats('general');
-		need_separator = true;	
-	};
+		this.updateStats('general');
+		this.need_separator = true;	
+	},
 	
 	// Updates stats by category
-	var updateStats = function(category) {
+	updateStats : function(category) {
 		for (var key in constants["stats"][category]) {
 			var el = constants["stats"][category][key];
-			watchStatsValue(key, el[0], el[1], (el.length>2)? el[2]:el[0]);
+			this.watchStatsValue(key, el[0], el[1], (el.length>2)? el[2]:el[0]);
 		}
-	};
+	},
 	
 	// Appends element to logger
-	var appendStr = function(id, cssClass, label, descr) {
+	appendStr : function(id, cssClass, label, descr) {
 		// append separator if needed
-		if (need_separator) {
-			need_separator = false;
+		if (this.need_separator) {
+			this.need_separator = false;
 			if (elem.children().length > 0) {
 				elem.append('<li class="separator">|</li>');
 			}
 		}
 		// append string	
-		elem.append('<li class="' + cssClass + '" title="' + descr + '">' + label + '</li>');
-		elem.scrollLeft(10000); //Dirty fix	???	
+		this.elem.append('<li class="' + cssClass + '" title="' + descr + '">' + label + '</li>');
+		this.elem.scrollLeft(10000); //Dirty fix	???	
 		while ($('#stats_log li').position().left + $('#stats_log li').width() < 0 
 				|| $('#stats_log li')[0].className == "separator") { //change to :first
 			$('#stats_log li:first').remove();
@@ -61,9 +64,9 @@ var Logger = function() {
 			$('#fader').css("left", parseFloat($('#fader').css("left").replace('px', '')) - $('#fader').position().left);
 		}
 		
-	};
+	},
 
-	var watchStatsValue = function(id, name, descr, css) {
+	watchStatsValue : function(id, name, descr, css) {
 		css = (css || id).toLowerCase();
 		var diff = ui_storage.set_with_diff('Logger:' + id, ui_stats.get(id));
 		if (diff) {
@@ -74,8 +77,25 @@ var Logger = function() {
 			var s = (diff < 0) 
 					? ("exp,tsk".match(name) ? '→' + ui_stats.get(id) : diff) 
 					: '+' + diff;
-			appendStr(id, css, name + s, descr);
+			this.appendStr(id, css, name + s, descr);
 		}
-	};
+	},
+	
+	mousemove : function() {
+		// Update logger when user moves the mouse pointer
+		if (this.updating) {
+			this.updating = true;
+			if (!ui_data.isArena)
+				this.update();
+			setTimeout(function() {
+				this.updating = false;
+			}, 500);
+		}
+	},
+	nodeInserted : function() {
+		if (ui_data.isArena) {
+			this.update();
+		}
+	}
 
 };
