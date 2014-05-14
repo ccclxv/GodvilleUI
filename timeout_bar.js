de49b93f335b438e23e2acee9846234d22ff5f79
VDSDmitry <vdsdmitry@gmail.com>
2014-05-14 01:37:19 +0000

    изменил проверку isArena на location

--- timeout_bar.js
+++ timeout_bar.js
@@ -15,14 +15,14 @@ var ui_timeout_bar = {
 		$elem = this.elem;
 		$elem.stop();
 		$elem.css('width', '100%');
-		if (!ui_data.isArena && ui_storage.get('Option:freezeVoiceButton') && ui_storage.get('Option:freezeVoiceButton').match('after_voice')) {
+		if (ui_data.location == "field" && ui_storage.get('Option:freezeVoiceButton') && ui_storage.get('Option:freezeVoiceButton').match('after_voice')) {
 			$('#voice_submit').attr('disabled', 'disabled');
 			var startDate = new Date();
 			var tick = setInterval(function() {
 				var finishDate = new Date();
 				if (finishDate.getTime() - startDate.getTime() > timeout * 1000) {
 					clearInterval(tick);
-					if (!ui_data.isArena && !ui_storage.get('Option:freezeVoiceButton').match('when_empty') || $('#god_phrase').val())
+					if (ui_data.location == "field" && !ui_storage.get('Option:freezeVoiceButton').match('when_empty') || $('#god_phrase').val())
 						$('#voice_submit').removeAttr('disabled');
 				}
 			}, 100);
