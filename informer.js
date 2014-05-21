51ebc0d1e893bd5cb349de35461f063ee1de4b1b
VDSDmitry <vdsdmitry@gmail.com>
2014-05-21 23:48:27 +0000

    исправлен 	Monitor

--- informer.js
+++ informer.js
@@ -6,16 +6,17 @@ var Monitor = {
 			ui_informer.update('much_gold', ui_storage.get('Stats:Gold') >= (ui_storage.get('Stats:Brick') > 1000 ? 10000 : 3000));
 			ui_informer.update('dead', ui_storage.get('Stats:HP') == 0);			
 		},
-		diaryMessageAdded: function() {	
-			var monsterWithCapabilities = false;
-			var monstersOfTheDay = false;
-			
-			// Если герой дерется с монстром
+		diaryMessageAdded: function() {				
+
 			if (ui_data.location == "field") {
+				
+				var monsterWithCapabilities = false;
+				var monstersOfTheDay = false;
+				// Если герой дерется с монстром
 				if ($('#news .line')[0].style.display != 'none') {
 					var currentMonster = $('#news .l_val').text();
 					var monsterTypes = ['Врачующий', 'Дарующий', 'Зажиточный', 'Запасливый', 'Кирпичный', 'Латающий', 'Лучезарный', 'Сияющий', 'Сюжетный', 'Линяющий'];
-					var monstersOfTheDay = ui_data.monstersOfTheDay.match(currentMonster);
+					monstersOfTheDay = ui_data.monstersOfTheDay.match(currentMonster);
 					for (var i = 0; i < monsterTypes.length; i++) {
 						if (currentMonster.match(monsterTypes[i])) {
 							monsterWithCapabilities = true;
@@ -23,9 +24,9 @@ var Monitor = {
 						}
 					}
 				}
+				ui_informer.update('monster of the day', monstersOfTheDay);
+				ui_informer.update('monster with capabilities', monsterWithCapabilities);
 			}
-			ui_informer.update('monster of the day', monstersOfTheDay);
-			ui_informer.update('monster with capabilities', monsterWithCapabilities);
 			ui_informer.update('full prana', $('#control .p_val').width() == $('#control .p_bar').width());
 			ui_informer.update('pvp', ui_data.location != "field");
 		},
